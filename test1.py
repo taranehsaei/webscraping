80382633a2eed785090d39d76a74f6f9794db59e
taraneh <taranehsaei@gmail.com>
2020-05-04 01:54:44 +0430

    update

--- test1.py
+++ test1.py
@@ -3,7 +3,8 @@ from itertools import count
 from bs4 import BeautifulSoup
 import requests
 with open("data_country.txt", "w") as file:
-
+    string1 = """ Country Other,Total Cases,New Cases,Total Deaths,New Deaths,Total Recovered,Active Cases,Serious Critical,Tot Cases/1M pop,Deaths/1M popTotal Tests,Test/1M pop"""
+    file.write(string1+'\n')
     response = requests.get('https://www.worldometers.info/coronavirus/')
     # print(response)https://www.worldometers.info/coronaviru
     print('response status code:', response.status_code)
@@ -38,33 +39,41 @@ with open("data_country.txt", "w") as file:
     print("number of trs in table:", len(tr_list))
     print(tr_list[0])
     # first country in list
-    tr = tr_list[0]
-    td_list = tr.find_all('td')
-    # print("North America tds:", td_list)
 
-    td_list_text = []
-    count = 0
-    for this_td in td_list:
-        this_td_text = this_td.text
+    for tr_number in range (len(tr_list)):
+        tr = tr_list[tr_number]
+        td_list = tr.find_all('td')
+        # print("North America tds:", td_list)
 
-        if count not in [0, 2, 6, 8, 11, 12]:
-            this_td_text = this_td_text.replace(',', '')
-            # this_td_text = float(this_td_text)
+        td_list_text = []
+        count = 0
+        for this_td in td_list:
+            this_td_text = this_td.text
+            if this_td_text == '' or this_td_text=='':
+                this_td_text = 'unknown'
+            else:
+                if count not in [0, 2, 6, 8, 11, 12]:
+                    this_td_text = this_td_text.replace(',', '')
 
-        if count == 11 & 12:
-            this_td_text = this_td_text.replace('.', '')
-            # this_td_text = int (this_td_text)
+                if count == 11 & 12:
+                    this_td_text = this_td_text.replace('.', '')
+                    # this_td_text = float (this_td_text)
+                    try:
+                        this_td_text = int(this_td_text)
+                    except:
+                        print("error:", this_td_text, "\n",len(this_td_text))
+                        this_td_text = 'unknown'
 
-        td_list_text += [
-            this_td_text
-        ]
+            td_list_text += [
+                this_td_text
+            ]
 
-        count += 1
-    print("td_list_text:", td_list_text)
-    for i in td_list_text:
-        file.write(str(i)+',')
-    file.write('\n')
-    # file.write(td_list_text)
-    # first_td = td_list[0]
-    # first_td_text = first_td.text
-    # print("first_td_text:", first_td_text)
+            count += 1
+        print("td_list_text:", td_list_text)
+        for i in td_list_text:
+            file.write(str(i)+',')
+        file.write('\n')
+        # file.write(td_list_text)
+        # first_td = td_list[0]
+        # first_td_text = first_td.text
+        # print("first_td_text:", first_td_text)
